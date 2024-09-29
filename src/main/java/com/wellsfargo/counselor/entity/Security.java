48911a75521e00be06b14d62b337f410504bdae9
Ananya Srivastava <ananyasrivastava419@gmail.com>
2024-09-29 22:36:05 +0530

    update

--- src/main/java/com/wellsfargo/counselor/entity/Security.java
+++ src/main/java/com/wellsfargo/counselor/entity/Security.java
@@ -5,23 +5,18 @@ import java.time.LocalDate;
 
 @Entity
 public class Security {
-
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long securityId;
-
     private String name;
     private String category;
     private LocalDate purchaseDate;
     private double purchasePrice;
     private int quantity;
-
     @ManyToOne
     @JoinColumn(name = "portfolio_id", nullable = false)
     private Portfolio portfolio;
-
     public Security() {}
-
     public Security(String name, String category, LocalDate purchaseDate, double purchasePrice, int quantity, Portfolio portfolio) {
         this.name = name;
         this.category = category;
@@ -30,19 +25,15 @@ public class Security {
         this.quantity = quantity;
         this.portfolio = portfolio;
     }
-
     public Long getSecurityId() {
         return securityId;
     }
-
     public String getName() {
         return name;
     }
-
     public void setName(String name) {
         this.name = name;
     }
-
     public String getCategory() {
         return category;
     }
